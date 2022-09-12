5c2470b285e53ee14697cadf1fa0f13bf70e89db
elek8871 <elekmichelle@gmail.com>
2022-09-12 15:15:05 -0600

    finishes part 3

--- index.js
+++ index.js
@@ -2,6 +2,8 @@
 require("dotenv").config()
 const express = require("express")
 const ejsLayouts = require("express-ejs-layouts")
+const cookieParser = require("cookie-parser")
+const db = require("./models")
 
 // config express/app middlewares
 const app = express()
@@ -9,11 +11,29 @@ const PORT = process.env.port || 3000
 app.set("view engine", "ejs")
 app.use(ejsLayouts)
 app.use(express.urlencoded ({extended: false}))
+app.use(cookieParser())
 
+// custom middleware for cookies
+app.use(async (req, res, next) =>{
+    // console.log("hello from mw 🐸")
+    // if there is a cookie on incoming req
+    if(req.cookies.userId){
+        // look up userin db
+        const user = await db.user.findbyPk(req.cookies.userId)
+        // mount user on res.local
+        res.locals.user = user
+    }else{
+          // if no user or cookie: set user to null in res.local
+          res.locals.user= null
+    }
+    // move on to next route or middleware
+    next()
+})
 
 // routes
 app.get ("/", (req, res)=>{
     res.render("home.ejs")
+    console.log("incoming cookie 🍪", req.cookies)
 })
 
 // controllers
