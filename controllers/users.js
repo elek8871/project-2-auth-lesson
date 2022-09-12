858e0b4feb2cd4be9ae9c516702de614550a3753
elek8871 <elekmichelle@gmail.com>
2022-09-12 13:29:11 -0600

    updates

--- controllers/users.js
+++ controllers/users.js
@@ -17,12 +17,32 @@ router.post("/", async (req, res)=>{
          const newUser = await db.user.create(req.body)
         // store as cookie in browser
         res.cookie("userId", newUser.id)
+        // redirect to the home page
         res.redirect("/")
     }catch(err){
         console.log(err)
         res.send("server error")
     } 
 })
+
+// LOGGING IN
+//  GET /users/login -> show a login form to user
+router.get("/login", (req, res)=>{
+    // res.send("show log in form")
+    res.render("users/login.ejs")
+})
+
+//  POST /users/login -> accept a payload of form data and use it to log user in
+router.post("/login", (req, res)=>{
+    res.send("log in user")
+    console.log(req.body)
+})
+// GET/users/logout -> log out a user by clearing stored cookie
+router.get("/logout", (req, res)=>{
+    res.send("log user out")
+})
+
+
 
 
 module.exports = router
